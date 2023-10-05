ba88b0c82f5e4834d852f7cf86b3f08dd4785bd0
RakeshMN1227 <RakeshMN1227@gmail.com>
2023-10-05 18:25:30 +0530

    First commit

---
+++ src/main/java/com/example/sampleDemo/SampleDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.sampleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDemoApplication.class, args);
+		
+		
+	System.out.println("I am start application");
+	}
+
+}
